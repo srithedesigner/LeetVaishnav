418fb91023481874a6c8ff7c41d9d846c7120250
srithedesigner <59601228+srithedesigner@users.noreply.github.com>
2022-07-27 02:27:30 +0530

    Time: 127 ms (32.12%), Space: 26.4 MB (30.85%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,37 +7,21 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        @lru_cache(None)
-        def search(root, x):
-            
-            if not root:
-                return False
-            
-            if root == x:
-                return True
-            
-            return search(root.left, x) or search(root.right, x)
+    
         
         
-        @lru_cache(None)
-        def get(root, p, q):
-            
-            if not root:
-                return root
-            
-            if root == p or root == q:
-                return root
-            
-            if search(root.left, p) and search(root.right, q):
-                return root
-            if search(root.right, p) and search(root.left, q):
-                return root
-            
-            x = get(root.left, p, q)
-            y = get(root.right, p, q)
-            
-            return x if x else y
+        if not root:
+            return root
         
-        return get(root, p, q)
-            
+        if root == p or root == q:
+            return root
+        
+        x, y = self.lowestCommonAncestor(root.left, p, q), self.lowestCommonAncestor(root.right, p, q)
+        
+        if x and y:
+            return root
+        
+        if x:
+            return x
+        return y
+        
