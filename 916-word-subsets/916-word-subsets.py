9f78757c96dc36bfc5e891c093903b2fe71bdfc9
srithedesigner <59601228+srithedesigner@users.noreply.github.com>
2022-07-30 10:49:17 +0530

    Time: 1218 ms (56.00%), Space: 18.7 MB (38.67%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,35 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        
+        chars = defaultdict(int)
+        
+        for i in words2:
+            
+            d = defaultdict(int)
+            for j in i:
+                d[j] += 1
+            
+            
+            for j in d:
+                chars[j] = max(chars[j], d[j])
+        
+        ans = []
+        
+        for i in words1:
+            
+            c = defaultdict(int)
+            for j in i:
+                c[j] += 1
+            
+            b = True
+            
+            for x in chars:
+                
+                if c[x] < chars[x]:
+                    b = False
+                
+            if b:
+                ans.append(i)
+                                    
+        
+        return ans
