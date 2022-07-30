6346cebe65a4a0507464fdef1ab9e6bd966e6bb8
srithedesigner <59601228+srithedesigner@users.noreply.github.com>
2022-07-30 10:54:53 +0530

    Time: 1488 ms (36.00%), Space: 18.6 MB (73.00%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -5,11 +5,7 @@ class Solution:
         
         for i in words2:
             
-            d = defaultdict(int)
-            for j in i:
-                d[j] += 1
-            
-            
+            d = Counter(i)
             for j in d:
                 chars[j] = max(chars[j], d[j])
         
@@ -21,14 +17,11 @@ class Solution:
             for j in i:
                 c[j] += 1
             
-        
             for x in chars:
-                
                 if c[x] < chars[x]:
                     break
                 
             else:
                 ans.append(i)
                                     
-        
         return ans
