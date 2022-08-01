d6dffabcba893c14983f3475e12f761db3db7bd5
srithedesigner <59601228+srithedesigner@users.noreply.github.com>
2022-08-01 10:23:18 +0530

    Time: 40 ms (76.12%), Space: 13.9 MB (73.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,13 +1,4 @@
 class Solution:
     def uniquePaths(self, n, m):
 
-        number_of_paths = defaultdict(lambda : 0)
-
-        for i in range(n):
-            for j in range(m):
-                if i == 0 or j == 0:
-                    number_of_paths[(i, j)] = 1
-                    continue
-                number_of_paths[(i, j)] = number_of_paths[(i, j-1)] + number_of_paths[(i-1, j)]
-
-        return number_of_paths[(n-1), (m-1)]
+        return comb(m + n - 2, min(n - 1, m - 1))
