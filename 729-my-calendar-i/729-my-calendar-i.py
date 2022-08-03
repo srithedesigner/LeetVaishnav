1f52b75c79004e7988244dea8edbd101cada136d
srithedesigner <59601228+srithedesigner@users.noreply.github.com>
2022-08-03 12:11:44 +0530

    Time: 422 ms (56.64%), Space: 15.3 MB (10.23%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,38 +1,25 @@
 from sortedcontainers import SortedList
-
-class MyCalendar:
+class MyCalendar(object):
 
     def __init__(self):
-        self.sorted_list = SortedList(key = lambda x : x[1])
+        self.sorted_list = SortedList(key = lambda x : x[0])
 
     def book(self, start, end):
 
         pos = self.sorted_list.bisect((start, end))
-        if pos == 0:
-            if len(self.sorted_list) == 0:
-                self.sorted_list.add((start, end))
-                return True
-            
-            x, y = self.sorted_list[0]
-            
-            if end > x:
+
+        if pos < len(self.sorted_list):
+
+            x, y = self.sorted_list[pos]
+
+            if x < end:
                 return False
-            else:
-                self.sorted_list.add((start, end))
-                return True
-            
-            
-        x, y = self.sorted_list[pos-1]
-        
-        if start < y or start < x:
-            return False
 
-        if pos == len(self.sorted_list):
-            self.sorted_list.add((start, end))
-            return True
-        x, y = self.sorted_list[pos]
+        if pos > 0:
+            x, y = self.sorted_list[pos-1]
 
-        if x < end:
-            return False
+            if y > start:
+                return False
+
         self.sorted_list.add((start, end))
         return True
