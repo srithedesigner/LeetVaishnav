3ad1f6d9f3bcaf5b9130b61964f70c36e9c404df
srithedesigner <59601228+srithedesigner@users.noreply.github.com>
2022-10-03 23:17:59 +0530

    Time: 2794 ms (22.58%), Space: 26.3 MB (5.05%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,4 +1,3 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        
         return sum(neededTime) - sum([max(list(group))[1] for key, group in groupby(list(zip(colors, neededTime)), lambda x : x[0])])
